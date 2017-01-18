b52adbe5b0f99ab2d0c9217f48d294421cbed8b2
Irfan Shah <mail.urfi@gmail.com>
2017-01-18 15:47:06 +0530

    Add default values for non required properties

--- src/main/java/com/gojek/ApplicationConfiguration.java
+++ src/main/java/com/gojek/ApplicationConfiguration.java
@@ -1,29 +1,45 @@
 package com.gojek;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+
 public abstract class ApplicationConfiguration {
+    Logger logger = LoggerFactory.getLogger(ApplicationConfiguration.class);
     abstract Object getValue(String name);
 
     public String getValueAsString(String name) {
-        return getValue(name).toString();
+        Object value = getValue(name);
+        if (value == null) {
+            logger.warn("Config with key %s was null", name);
+            return "";
+        }
+        else return value.toString();
     }
 
     public Integer getValueAsInt(String name) {
-        return Integer.valueOf(this.getValueAsString(name));
+        String value = this.getValueAsString(name);
+        return Objects.equals(value, "") ? 0 : Integer.valueOf(value);
     }
 
     public Double getValueAsDouble(String name) {
-        return Double.valueOf(this.getValueAsString(name));
+        String value = this.getValueAsString(name);
+        return Objects.equals(value, "") ? 0 : Double.valueOf( value);
     }
 
     public Long getValueAsLong(String name) {
-        return Long.decode(this.getValueAsString(name));
+        String value = this.getValueAsString(name);
+        return Objects.equals(value, "") ? 0 :Long.decode(value);
     }
 
     public Float getValueAsFloat(String name) {
-        return Float.parseFloat(this.getValueAsString(name));
+        String value = this.getValueAsString(name);
+        return Objects.equals(value, "") ? 0 :Float.parseFloat(value);
     }
 
     public boolean getValueAsBoolean(String name) {
-        return Boolean.valueOf(this.getValueAsString(name));
+        String value = this.getValueAsString(name);
+        return Objects.equals(value, "") ? false :Boolean.valueOf(value);
     }
 }
--- src/test/java/com/gojek/ApplicationConfigurationTest.java
+++ src/test/java/com/gojek/ApplicationConfigurationTest.java
@@ -4,6 +4,7 @@ package com.gojek;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ApplicationConfigurationTest {
@@ -21,6 +22,16 @@ public class ApplicationConfigurationTest {
     }
 
     @Test
+    public void shouldDefaultGetValueAsStringAsABlankString() {
+        exampleConfiguration = new ApplicationConfiguration() {
+            public Object getValue(String name) {
+                return null;
+            }
+        };
+        assertEquals("", exampleConfiguration.getValueAsString("test"));
+    }
+
+    @Test
     public void shouldGetValueAsInt() {
         exampleConfiguration = new ApplicationConfiguration() {
             public Object getValue(String name) {
@@ -28,6 +39,16 @@ public class ApplicationConfigurationTest {
             }
         };
         assertEquals(Integer.valueOf("1"), exampleConfiguration.getValueAsInt("test"));
+    }
+
+    @Test
+    public void shouldDefaultGetValueAsIntAsZero() {
+        exampleConfiguration = new ApplicationConfiguration() {
+            public Object getValue(String name) {
+                return null;
+            }
+        };
+        assertEquals(Integer.valueOf(0), exampleConfiguration.getValueAsInt("test"));
     }
 
     @Test
@@ -41,6 +62,16 @@ public class ApplicationConfigurationTest {
     }
 
     @Test
+    public void shouldDefaultGetValueAsDoubleAsZero() {
+        exampleConfiguration = new ApplicationConfiguration() {
+            public Object getValue(String name) {
+                return null;
+            }
+        };
+        assertEquals(Double.valueOf(0), exampleConfiguration.getValueAsDouble("test"));
+    }
+
+    @Test
     public void shouldGetValueAsLong() {
         exampleConfiguration = new ApplicationConfiguration() {
             public Object getValue(String name) {
@@ -48,6 +79,16 @@ public class ApplicationConfigurationTest {
             }
         };
         assertEquals(Long.valueOf(1), exampleConfiguration.getValueAsLong("test"));
+    }
+
+    @Test
+    public void shouldDefaultGetValueAsLongAsZero() {
+        exampleConfiguration = new ApplicationConfiguration() {
+            public Object getValue(String name) {
+                return null;
+            }
+        };
+        assertEquals(Long.valueOf(0), exampleConfiguration.getValueAsLong("test"));
     }
 
     @Test
@@ -61,6 +102,17 @@ public class ApplicationConfigurationTest {
     }
 
     @Test
+    public void shouldDefaultGetValueAsFloatAsZero() {
+        exampleConfiguration = new ApplicationConfiguration() {
+            public Object getValue(String name) {
+                return null;
+            }
+        };
+        assertEquals(Float.valueOf(0), exampleConfiguration.getValueAsFloat("test"));
+    }
+
+
+    @Test
     public void shouldGetValueAsBoolean() {
         exampleConfiguration = new ApplicationConfiguration() {
             public Object getValue(String name) {
@@ -69,4 +121,15 @@ public class ApplicationConfigurationTest {
         };
         assertTrue(exampleConfiguration.getValueAsBoolean("test"));
     }
+
+    @Test
+    public void shouldDefaultGetValueAsBooleanAsFalse() {
+        exampleConfiguration = new ApplicationConfiguration() {
+            public Object getValue(String name) {
+                return null;
+            }
+        };
+        assertFalse(exampleConfiguration.getValueAsBoolean("test"));
+    }
+
 }
